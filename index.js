ad646930961cda9510d7ce66bc2a5d2f0355e7b6
Al3nMicL <https://twitter.com/Al3nMicL>
2019-01-27 20:18:07 -0500

    first two tests pass

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // add solution here
+// The Beatles Play
+let theBeatlesPlay = (arr1, arr2) => {
+	// arr1 = musicians, arr2 = instrument, arr3 = phrases
+	let arr3 = [];
+	for (var i = 0; i < arr2.length; i++) {
+		arr3.push(`${arr1[i]} plays ${arr2[i]}`);
+	}
+	return arr3;
+};
+
+// John Lennon Facts
+let johnLennonFacts = (factsArr) => {
+	let i = 0;
+	let j = factsArr.length;
+	let k = [];
+	while (i < j) {
+		k.push(`${factsArr[i]}!!!`);
+		i++;
+	}
+	return k;
+};
